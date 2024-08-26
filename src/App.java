04fc6451e3385a96d614beb4c2edc0402a61eb78
luis-jmtz <luis.jmtz2002@gmail.com>
2024-08-26 16:08:59 -0700

    Created Screen for Game

--- src/App.java
+++ src/App.java
@@ -1,10 +1,22 @@
-
+import javax.swing.*;
 
 
 public class App {
 
     public static void main(String[] args) throws Exception {
     
+        int boardWidth = 360;
+        int boardHeight = 640; //integers are in pixels
+
+        JFrame frame = new JFrame("Flappy Bird - Code Practice"); 
+        frame.setVisible(true);
+        frame.setSize(boardWidth,boardHeight);
+        frame.setLocationRelativeTo(null); //places the window at the center of the screen
+        frame.setResizable(false);//keeps user from resizing screen
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        System.out.println("Frame Created Successfully");
+        //time 4:24
+
 
     }
 
