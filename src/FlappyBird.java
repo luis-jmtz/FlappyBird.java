be7ca633047dedf71b466a8a701897a95036993e
luis-jmtz <luis.jmtz2002@gmail.com>
2024-08-26 18:16:49 -0700

    Loaded in the Images and "drew" the background

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -8,18 +8,41 @@ import javax.swing.*;
 public class FlappyBird extends JPanel{
     //basically makes the class a JPanel object ^
 
-
     int boardWidth = 360;
     int boardHeight = 640;
 
+    //variables for the images
+    Image backgroundImg ;
+    Image birdIMG;
+    Image topPipeIMG;
+    Image  bottomPipeIMG;
+
+
     FlappyBird(){
         setPreferredSize(new Dimension(boardWidth, boardHeight));
-        setBackground(Color.BLUE);
+        //setBackground(Color.BLUE);
+
+        //load images
+        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        //because I used getClass() I only needed the file name as the path for the image
+        birdIMG = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipeIMG = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeIMG = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+
+        
+
     }
     
 
 
+    //methods that paints/loads the images into the JLabel
+    public void paintComponent(Graphics g){
+        super.paintComponent(g); //evokes the paint function from JPanel
+        draw(g);}
 
+    public void draw(Graphics g){
+        g.drawImage(backgroundImg, 0,0, boardWidth, boardHeight, null);
+    }
 
 
 
